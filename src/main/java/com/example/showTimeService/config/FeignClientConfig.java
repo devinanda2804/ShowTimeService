f0167cdaacc96e41f4f78643f6d702411f138c83
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-11-05 14:23:53 +0530

    Added FeignClientConfig and GlobalExceptionHandler

---
+++ src/main/java/com/example/showTimeService/config/FeignClientConfig.java
@@ -0,0 +1,4 @@
+package com.example.showTimeService.config;
+
+public class FeignClientConfig {
+}
---
+++ src/main/java/com/example/showTimeService/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.example.showTimeService.exception;
+
+public class GlobalExceptionHandler {
+}
